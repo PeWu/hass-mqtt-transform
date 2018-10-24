0b48ccb708c0b2b8debe3b4b2e3940897a8f7522
Przemek Wiech <pwiech@gmail.com>
2018-10-24 18:47:59 +0200

    Converted constants to command line flags

--- hass-mqtt-transform.py
+++ hass-mqtt-transform.py
@@ -1,34 +1,62 @@
 #!/usr/bin/python3
 
+import argparse
 import json
 import paho.mqtt.client as mqtt
-
-# TODO: Convert to command-line flags.
-MQTT_HOST = "localhost"
-MQTT_PORT = 1883
-EVENTSTREAM_TOPIC = "ha/events"
-STATE_TOPIC_PREFIX = "ha/states/"
+from functools import partial
 
 
-def onConnect(client, userdata, flags, rc):
-  print("Connected with result code " + str(rc))
-  client.subscribe(EVENTSTREAM_TOPIC)
+def onConnect(topics, client, userdata, flags, rc):
+  print('Connected with result code ' + str(rc))
+  for topic in topics:
+    client.subscribe(topic)
 
 
-def onMessage(client, userdata, message):
+def onMessage(prefix, client, userdata, message):
   payload = json.loads(message.payload.decode('utf-8'))
-  eventType = payload["event_type"]
-  if eventType == "state_changed":
-    eventData = payload["event_data"]
-    entityId = eventData["entity_id"]
-    newState = eventData["new_state"]
-    client.publish(
-        STATE_TOPIC_PREFIX + entityId, json.dumps(newState), retain = True)
+  eventType = payload['event_type']
+  if eventType == 'state_changed':
+    eventData = payload['event_data']
+    entityId = eventData['entity_id']
+    newState = eventData['new_state']
+    client.publish(prefix + entityId, json.dumps(newState), retain = True)
 
 
-client = mqtt.Client()
-client.on_connect = onConnect
-client.on_message = onMessage
+def main():
+  parser = argparse.ArgumentParser(
+      description =
+          'Transform Home Assistant Eventstream into separate state topics.')
+  parser.add_argument(
+      '--mqtt_host',
+      default = 'localhost',
+      metavar = 'HOST',
+      help = 'default: localhost')
+  parser.add_argument(
+      '--mqtt_port',
+      type = int,
+      default = 1883,
+      metavar = 'PORT',
+      help = 'default: 1883')
+  parser.add_argument(
+      '--eventstream_topics',
+      required = True,
+      nargs = '+',
+      metavar = 'TOPIC',
+      help = 'topics to listen on')
+  parser.add_argument(
+    '--state_topic_prefix',
+    required = True,
+    metavar = 'PREFIX',
+    help = 'topic prefix of published states, e.g. ha/states/')
+  args = parser.parse_args()
 
-client.connect(MQTT_HOST, MQTT_PORT, 60)
-client.loop_forever()
+  client = mqtt.Client()
+  client.on_connect = partial(onConnect, args.eventstream_topics)
+  client.on_message = partial(onMessage, args.state_topic_prefix)
+
+  client.connect(args.mqtt_host, args.mqtt_port, 60)
+  client.loop_forever()
+
+
+if __name__ == "__main__":
+  main()
